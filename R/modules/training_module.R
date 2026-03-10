46c01d0d3fbe7143dd1213e31525c622fa6e5392
MadsWKofoed <madswiedk2000@gmail.com>
2026-03-10 10:57:33 +0100

    Fixing legend issue in roc plot (nrow = 3)

--- R/modules/training_module.R
+++ R/modules/training_module.R
@@ -729,7 +729,7 @@ training_module_server <- function(id) {
           y = "True Positive Rate",
           color = NULL
         ) +
-        guides(color = guide_legend(nrow = 2)) +   # <-- NY
+        guides(color = guide_legend(nrow = 3)) +  
         theme_minimal(base_size = 13) +
         theme(
           plot.title = element_text(face = "bold", hjust = 0.5),
